7d93b0a0119a29f0f259bb42a9abcb4ba18495a7
Chris Manning <cmanning@spotify.com>
2022-12-04 21:00:30 -0500

    commit previous day challenges (1-4)

---
+++ src/3/2.js
@@ -0,0 +1,61 @@
+let input = ['DPstqDdrsqdDtqrFDJDDrmtsJHflSJCLgCphgHHgRHJCRRff','BcBGcQzVBVZcvznTTTvZcGTpCRRRfRCggLflHlhhCZpZCj','vGQnQvnzTzNTTbVnzGBqMqwqDLdPtMmbwqqLLM','wLRFRqvFsFRjfrHddbdbjzdH','lcsnSJPSSVVlGmGrHzbbrGNrdzbz','mSmlnnPlmJmncVDSlSZSlmLBCvtwBvtLCqqswsDBCTWW','pfqPrPgmmhvqdlsdWq','nfjHLJfZcLbVtQWWtndhls','CzJJFLzRzfDwrmggpC','CWfllmlCDFlZZqMfmFBWmWLJVRLVwNNtRVGPpwtGpqbJ','jHndndndcjhscnhHNtRbVtLbGpJbRRcb','HSrvnQzQSMDlLzBCfg','BQRVbgQQBJBbBtVBSSSRWMQbdNvvRPjZjCCdPLNZNNsNCCzd','HwpFpnlGpGZWGvjzPd','FTDmFrrwDpFMtmQVQQcWgc','VhbPshVDPDFWhWsgDNMMbVtmBjwBffpwBntnmnqfnswt','QzzGrTZZdrdlTcCLpRBnmBRRjBCqtptt','rJdGlmLTJdJrvZDbSbDSWDNbFJgD','qrcqTBHTcHgwWWdHRjdWBglBbGPpGvvPbszGzsbPpQfPLwPz','nFVmjhMjFJCSJsQQPQLbLpzCPQ','SnMSVVZSJMNMZNDVFtJtNRBdqBrWrRHWZTllrrjgHq','ZqdqcrPqqrwnQqnrZqjVcqrQwwmNbzNzwbNLzvFbHLbNmBLF','LCDsCsRTfLTDszzNbsbNNHbs','gLfCgShfCgMPlPrVcqrQgn','QSNSLDQDLfqqPwwBNLqgqJMMmmRRCTzHnCHhRzHmfCmh','lGvdbdWdVvsVszpDhHmmlnMpTC','ctbdtVsbbvvsbWZFdVQJqPtgLQBDBwBQPJwJ','dggSSDCPddRWPnSSPWRDgdSrTDsDQDTzQGGTMbsMMVsQfTfV','jmBvtFpBcBhhjljZHphztMsCbsTTCbzsqGqsfz','hccpLmhFlcwCrPLrCPnL','MMHZnGrCfJnfCPggSSGGSSSgLW','qhFhRlDFDlqFsgdvJdWdDdcSvp','wVlhqTbbRNFqswlVVRNbZfHCrntMBrTTZnJMCfnn','sHGZscVGHJMtmRrqzRqqqTqt','SjvvNgjLShWWhhSQNWqmrBzlRllTTgBqnRmq','LNQWLfWhSQvLdCddWWPHMcbHHrJcDGFZCJssFM','mSDjSVQbVGbmqDVbHmqqJTZzPHTHhhRJhwsRPcRJ','tFfFFttFdsNntfpMMppJWwZTzJczJcZPzwWcdJ','vNtgCrpgNptptgCCbbmjbSbvsVjSsjGG','VCQlZJCTPRWsBsjTTT','wvNrnbbvnhdNhLMfGsrGpRFpGpjp','dwndHbHbbLqwwhNcLsqSHSCHJClQtJSttQDPSJ','ZlrvrdvpGBBhlDrshdqJHRPHqPTJzRPPqw','tcftfSgFFgcgLPmPmpnqFwJRHP','pWLCcWNNNNttMNgZvlsvrBrsrjWDjB','wgdCJgDMDwMCwDMCMJsJJfpffVpVfbfrrrrgjgllZp','QFRhvttRthtQzzmpBWbWzWZSVpbSpl','btRttRLttGNqvbFHLwCdDcMwnPPJDnDD','VhmMNllLqGLJQNhRfZHgSPfgSPTqZj','sBwDcwBtsdzvvHZRlPRjDZTgPZ','pWvvBBcBCdzNLVQVWQlNlW','NsSppvSjSPNBNLJJLh','fCGtqQbZZGZQZTbtzbqbCZThddcMBddlJGhdlBMcddgLlJ','zZFTqwLtTRFqTQwvmprnRVSsDrnvVR','FttFTzzvlVHFzTjpbvzbFSDDdVGhdqLGWGJdVDDfsLqG','cmBNCRnwsCcBPMfLLfJGcWhWqfdh','BwPmZZMmZMCsnrwMrmbHHbjbSvSbjvrlHpzj','sZQHCBFHQQQPGQCCHCHwsHFshhtSnnqjbRSSPngnhbRjqVPn','mzLvmDvNNWvNvrzzrMTzJNjqndqbnSnnRgTdtjdbjhTt','WzzWDlJLzLDvMWJJlMzmLJWcpHFQBpBgBHGQGBHfwwBfQQlG','gdpFrdrmrDsqqswdtccgWWCMlChSbhqSlCzBlSqh','TTvjrfjNJPnRQNTQjvNnCSWBVBVbClPSVSbSVhSh','HRvnfTfvjjHZTDsmcHDsrDsdmp','bFChjhbpbjqsntjtns','vdWcfMHfddvrlNMNdWWTNgBqDngBBZBBQZshgSfgnD','JlwrlrlhlcJWcWMwhWNVFpLzwPbbLRFPppVLzm','DtBtgLvgcHzllsTwzSTg','vhhjZrCrZdVdZVSwPMwwTTMGwT','nmpfqrnZJbqBBvRc','nMvSLvWSWPVPvWnSLShFLBjVbpNVGGbVQbbNcBcBBc','sTzJsJszbbQbdQJb','DsDrwTtsCTFhLQSShRwh','RNFQhTQqHNNGRsNqQFNsHhFCwwPLwPqwzfPrrPBwpJSJJw','vMMMblZjddlvWbjbBBfbwCrPPLJppwCL','jDmvcDBlBdjVglgddmvDQRNFtFRGtQhstHNsGFHV','rhLHmZnMrRsZSstZLLtZnhSCNbbmPJVcblTNNTlccpNTjJTj','WFgGddGFFgFDddMblpJjlTJTPc','QGWqBBfWgBqWwFwzMGvzDqSSrHnCHsrssCRZrfRhHLfH','HHzcWqNPmZcqFHPZGBdMRBMDlllWpRDJMl','tTgSvPhbMDJlbJQb','SCTtvtSPftswjvPhTgffVqmGZLmqmCCcqZzZHFNznF','QNpppRrdZvdgzpQZNpgRRgbSwmDDvFGGqwJSsvSGSqGG','HchWBMcBVnnWcHPjHhWcjHTqJFDGMSSqDMwJJbGwSsGGSb','tcCVcBjPjhnWlFrCFNZflQNr','HsVMrqrPqvvgprSrLG','THJWBJDwRFvBgGSzgF','DmhfHnmQncMNVMqPqbcd','SqZmMJqvHJBhHJLp','wsgTVTSsPssjjFVrTrFlhLhCFlBBnHplHLLHfF','zgggdwPrRrsrjjgRwVwdwdQTmvMvZqDZbWqqMSNWNbGQbGZN','fBDBfLZnTLZVVmmDcQMDDV','jPFtJFpHpJqfJFrptwrJdRWRWNpQVmQRMWNVVVNVvQ','zHwJgtFrTlslfghf','wMwTttCCTTSTfBmPzPVZnPZLVVtbnN','ldRRRlRHggGcvcRbZsNzvBVWnnPBWv','hdlJHgpcJccJhQdJrcrhwFMpDqCwCMBqjSqjqpTC','fJfnwJJnnHJgJHTgjsjDccNjcbgNjm','VdLqRRqGVqpRrPpppMBjDNmDctdsBlNjmZdZ','PDQvPQSvpGDrTwfJzzfFnTnS','MnHvnHHMRMzPTlDLPPRGcl','dFnfhFVwhdBPBfGWlPcP','JNrQFsnVtwsgvNzvmMjpzS','BZVPFpNpcNZpmRRPpzcVNhLLnssDjjDGnqjjLDFDjq','mMJbJvtJQQHlJDGCDnjvChDSsv','MQwWJHdQwWrJltQrgfNPmfBcBrpBpZ','ZWZqDsZZqWsWvWLPwPbpHjdtSbSjSCSPPSCp','MFVNMLmFmNzcTTrFrLbjdjbpCdCSbTCShRSd','czNzLrznlGNNrMzMwDlJwJWDwJwqJDvW','GlgchGGVShlQcQfDhzZrNFnFNFNjFzNFcn','dwCtpwHTtPTWpdFNfJJzRzvJNR','tLBBmWHftBttPbLwCHWTsSSQVglqgMsMBDSMGlQS','RDDDGhGfvPPTTPTThn','ZFLMmjpCpfMZzFqmqsCmPjdVBlVBVnWBPNTVbnTV','zHqJMCzLvftRQQHG','nTcbnvPsvdvFzpczVZmMGg','BCCJwSDqhQLJmMMpzGZVFVFB','qhrwJwrJrrzJNqwWLsTTnlTlbnsvbstWsW','vHRbqPJZvRPZhShJvTZllZtgzwlfBGBlsm','VdQjVVCssQVrWrQmTBgBzglmgCBGml','NnpQNpcFpNWshPRLsbSFsH','cVGmVZVwVVMLdvcRttTdbB','ppCQrwzHBtLrttLb','hsFJQzFWCpCqjZGVwlhlPP','HDGRzgWhgfzVWfRpspwRwbwStSwt','ZBPPPmmmTMQMPcZrBmSptSbbQCwtlsNqCwjC','TTLMMmZvPTrMZvFMmcmvrTccDDnfGHJgJhHhnhnLfVhSWDJz','pNrpjzthZPnGrzzWbJLLLbbJZwgSvZCV','MQsFFFDTfMNfRFfBFMdBdwLSvgbSTVCqTgVbbTLvwV','BQlQDMFccQsNmWpPGhpcjr','CTgGRCRglLlLTllL','vMJmhPJcmPBMvhqPDnNNqlWnwDWqsRQs','hcBfcJRPFfvvRvJZBrfMPZdpbSSGtSdtdgtzzSZzbV','NQLzNzzJcrLrSgZSSGgZrR','bTsjqHvcmTHvjgZGDvDpGZRfpg','WqTVPbdnMlLJncQC','hZLBrqLGLMbzLLBhfMMrnnNJlnNnlnJJNNdCJdzN','TWTsWqvtvpTSgRHpVFdjgjCPdgJlCFCFnF','swSTsTwpTVRmVRRRqMDMfqfDwLfbrhLr','NTQHWNQWrQwSTDWlcPPBHZBZbPgZJZ','nmfjCRCfRhndJcjBbcbcbg','nsppRfssfzCnqgzTzrwTwQVTWM','mFjQmDGmbbGjmChrCwdQBHCHWh','qvZZnPvvnngMpnlqpMZnpsTgWHTRCWrVdVDBWRhHBrhHDHhd','vqZgnnqgLvqlPllpnjGDjmNjNLfftftLFD','rfGsjsMNnFMMFddMsttDMgLHGlmJLCPPmHHGmHPlmm','vZcbhQbrRbVZPJLwPTgCLlgb','hchzSBqzQvphnWnrjFdWMqff','WmfPWfVsfqszRDqPqgpvHhvdwddGMmGghM','QtTrtTcSBjtQCctStrTrzhpwjGvGHhngwMHGHvMv','TtTQlQFcSSJlcccBbltQQTTRsPZDsWzRFzWFzPNfsssLPs','QpNNMrjcNMccGNdvLBBlBsBjnsnF','tTSqbbbqCtWWCTWSVTmmCJPwVwnwvFPnsPVnnPfddlvf','HmhJTZWqHqCJJJltqpNGRgzZDcQNrgzDGM','HcLVRhhTRsLRRVjslTscqNQmVNQQgQttqNwNZtmw','nJdBJJhfFPSCbJBJBMbzFbFgmNmtgmvgNgnntNwZQQNNmw','bMbPzJbzCBPrJfdfbBbdCrGHlLTTpWjsGhGTTRTRlc','sJCCpQJQCrfCfnSCrT','vmqgNggzgmZqmPShqBhThfhDhjDhhB','RZNzHRzZSQwHwHVVWc','jtVtvVHgvjJbHjjQPMZdCcwlMdNbdFlNlc','WppSBDzGfBzTBqQWwCFMlwZMwMcZ','zBfnqpRGnSSqTfqpTpSnnHQsjJgQvPJshHtHVh','qJMRMcPPVzVhmsDWfhWT','BglQBNlgZtQBHLHHBnTjWSWmFmwDmWjSsnmF','BdHvgHBvBtZbTpJRPCdcdpGrGJ','pcGcWGWlvQZpzmDbgFmz','HqqnddDdddjzTTggjZgFtT','sHqRwrRsJswLHrMLLRJdqNVVrGffPGWcvSSWlDfGfc','lttTbgRvqvtQRhjLzGjLVh','JJfrHfrdffZJQmZhLLZVVwFj','sBjCfSNNTTqnCnqD','qMtWjSrHftGfjqrJGMqzVzFmBBrzQQwzgBVQVQ','LDChPbThbTcTpCTcnPPQPQzVPvvzQBBWgVBQ','ZLspppLpdZZttdHttqdWMf','htJcJhpMQQWjhNWdJQSCFCTvFBPCTDlMmDCFlM','jjbbsfjwZbLGVVqHCFPvmvDmClTfmP','zjVVRwZwnRJtnNQt','PCPVSzLMMRqGwgMmHmQmDQ','slrrbZZgsfcdsgdhrHFGQHQFwvfwFwDGTv','NclhgpctrrNjllcZdcrpZnPPqzLLSSLqJLtJWCWzCn','PBLSBPVBwpTVppfT','lZCqQQtCQGPJJPtPHHwTwZTTZpwHsfRH','mCtGFDqFGDGQjPGqjJMMlqPgdWgSSgBWWcWzLdgvMzgBMg','cLBrfchhFBcnrgvqvPGvvwSS','QpzpstDDZMwDZqwh','WzpbWTjsbhpQtjThsjJFRNLnfLbfRLRBLlFB','ngnWWqnfgqtfsrWftqsrFWPSdSSdRCTHRSwpRGTfGmSG','VhJhVczJQcvbvvlhBpvlPdmlwHRTGHSRPTSCRGTd','zVcBcMhzcVcvMJJJDpWrsqrtrWLWsgZZFtFD','fbccrJlrffTwJDJTtBtB','hRNNFddsgsFPLLRVVwthMCQTtBwrtT','jrsPGLNjsqPlvGbZbcvScz','HFPmmgQrQzFgrLVPPrLFPNDJNJzzcGbJTbsSzbGGNc','MtvCMhJBdnMhwfhlwnfBfMDCDSjGbqDNGNGqGjDDjsDC','wwnhhdtBBptwdlhlRntRldJFVWRFPmWZFmHRZZmFQPRWmm','WrHNNTBNTTTBwHHcSTrBnSzJPFnpJfpLVfpDVdJLFJLFdD','hRthQvhRQlQmDpfVJFdLlLLj','hMZZbCMvgQgBTBGNGDcWbr','HvQjMRMTzjsCQzHTCFfVVZLPVvfLfPVpZg','GtlbBtSGlSbDdStrhSFCPVDgZgLLgPpPJWPF','rmwSbcbcdbrbGljQjCzCCwnHRqQT','bbgNSHPPgnmMMZtNcMpp','VFzFDFVtCBFDCVFdMlhZMhdhmhmplwZL','JVtBjGRFRttFCGDFGJJDQQgSgTWPPfSSfWbQnHvPWW','NvdBpwNvGNFvpBGGBmLFblrtVTwDttlhtlblfQbQ','SCMMsWCMSRZCqsmWcRWgRRsVtlrtrQbtQftThrQTQtqrtQ','WRscMgZJJCJWzZgSWNLFdBNHGzpFGmBFFG','qghqRVzhLNRLqzLhVztgQdLFdrccCnSpcZdSZcTS','DwvmHDJDsmvDGmHbQBlslMDDCrTCnppTndrdBrFnFTSdCCnZ','GGwJHlGwwvMHJljwwDMVtfhtWWhzqVPPjfQRqz','BsDMPrqPzsDwwCLGmqjpjm','VfFJQlVQcvfwJLJCJppLNp','vfcSHCglCgbgbbbFvSlvQfPsrsZrPzZzDWWStPhtZPDP','gjMsnFgbnllbjMfSZBHHtpHvvvFwhv','DDRZDLdVCLNLJwBCShQHHwwBzv','DNJNTLJRTqWmjWZnjrlmjW','ZTSVSFZCLTnvzfzqvnNL','PfPcJljfMpvtlnztvQtw','PsJMMMWpGcgMHMfjRBThrgrTbBSBFdVSTF','GccBRWjgtQqsTcVQcw','JhJCMJHPLffMChlfLCLHMMrDQsQqDVQsqTDbVvGqDhhzqD','dHGlfnCHlJrNmtdptggpmW','wnDDSBCSBSDLzLLmHLrlwlmpTTqzGJJfpjfjNpfqbpbdpG','MMRhFWWRvZPZRZQhFZMVhVSNqjqpNQffJjbjfbjdTJbp','VMMsWcZRWgMPvRSrSHmsrrtwSHnr','TQchPTgjBcNgPHhhThtNzQdzdsCmRDJnzCCmCdCm','vllVwrfvbSBVFSbVGwlrFGlqRCDzRJCJdzvJRzsdLDDLsdCm','VrMrqSWbfqWbBhhpWjNTttpjjP','rsfvSHHcvwrMPtcQZgnDhGdvJzngLzzJLJ','lWmVlfbCCNFCpBCmTpFFJgzhDLGhmRGhLhmGdgGR','pNBfflVTNpfWTWbWWbjNVqBsscqsqrZSwMwZrMPZSZrZrs','PJPHPJmhhHhlHPQgCndngTbWnqCWDGTD','tSwccFpFqwMcFbGFWvnnWvCW','MwLwLMSwpNBBtctSctfhZHJQhhqmlRlZRNPH','GNzdZhVGvtGZVVgGgtfHHWhpLPPpLWpWWnHf','RjwqRcDTvCrWJWWnlLnnqn','DbrDDwwBwjjsrbDTRTBmwgZmgGgmdttvQvQFSQGFtg','jRgcZRfhmHfZjPZRgHffLFTzzddBTBBFzLDZzBTF','VtsJwSbcStlwMqbtwbvWBWddGGdrFDDWJWrzTT','VwsQQvlbVbVlbNllVwbMmmpnjpfChfQpnhfcCCnH','dFnFjWjTQTFzFWPWPgqhRQRqgVhRqfRqQJ','bStrbpmNGHSrBDmrNBtHBhMVLLqLqVVglrllPVLgPg','tSsbBDmBbbGmmSHDbtmHbtNCjnzscZccjnPcTcdzWcvjswFz','lFCjDhqggMlDvMhFDgqFFzHHwHwwwTpLBwmwqmmpBpwT','GPdPnStGncQGNStZPpBmVZmRmfzTRmVVfL','tWtNdPWzsbtMDCbrCbrjrv','BJHMgLlcMTBLCtbqmMDGppmmMM','ZFPsrrdvwrNvrdNZsvhrrzzRSmJRbJSmbztsmpRSSm','NwhfPZFNdFQPVQdvZFNgjglJLTCQngQWllBcTT','jGlQQvQvpRQRGfnPLfcfGTnP','BMqmdBVBwmFdVMFZdcTPqgLnnggTTLSzPS','FVtMMVcbZVrcZMQCHjHWJJCJDvrW','rPPwVwbpRbbVlllTLCTRqTLL','dNdZssBBCBszHsjhDTQgqLDvlTgDZgll','dSsCNNHMdsdWWWmpGfmPFS','rzCLrsjgZjwcwSZc','wNBNRJpRltHNWWRHBlGlJtRcTZSVBmZDVqZTfBVVTDTVTD','NWPtGJPNGWHvpvtwvWgzQvdvQQzhnsnCvCLM','HHbJhzddMPbPgnDWbZ','BLnjLNvBrrcvvvwnwLrnqrgpPRgRNCWgZDPPpDgRpRWp','jtsBqScStfJQnnVF','QVFSVgQFZZQlQqQSlgQpRppSbRTSTppJJbRpLb','cGwCDwjrnrGvzBzGnwwvDBjnpLbsLTTqRPbsJPMJMWpPns','tcGzrCdtGdQmVZqVNQ','RtTRhncVMTVccShRTctLdfPdJpLPqJhZphHpJs','BzssCmFNWWqWwqwPLH','svzvvsmmFBmsggrGlGMVSMtMRRncSQScRRRl','rmmqrQQwLbbGrrGr','cNJzzzWtWmLCGGbLWWbv','cVtMppchzMBVMcNJcMsRwqZFMlgmggmRgg','mQsQBHFMrbddbRqH','NzhcQNfNNtzvWwZdSrgbrprPrwLbgb','zcJVhTtNNcvcfVZmBmQMGMMljTCmlB','FlldqjSlCgfvPFfvFF','rbnDtVBMbprTsbVVcTDTrpMcmNwgHPgghTmNfLwvfPNLwhdT','drMppdnbbtQDBtbnsBbcrrtbSqSSRCjlQZWllllSRlWRGCCC','nqdCsqbbwdsrHFVJHcwFTc','jPPjtWjPWgRltRLsBRrNpHFDHVFWVVJNNHrD','fgllPGQjBffLjtzSsqvbSSzGvnhS','zsVBzMfHHnzlwwVlqcJJFT','ZzRLvLDzQzTmlWlqWRWF','GbQQvpGvSSpjdQjSQZpQZGLfrgBCsHzrdtCsnfCBHsBgdH','zBLbLWzqqwLMnMZTnHlnsHTvFlFHNT','fjhdcrjjdVdrGSmmdfccGclPvlvPTlGHTFgNvNgqFFvg','pmmcrcRrjSVJchqVccjpRwZMDwCJQBbLDCCbwBWLzL','TDMBgBgLlcjBfMfcVJVmGnnJjvPVCPVv','zzptqHstJqFzzdJJZNvNpvNpnNvGnNZm','dHszrWQhdzHQqdztwQBLSfglfDbfJlJTLg','VTmvrldtGGwmlvmGDHlLnFDCCplFQHLH','ssgjzSzzJCQSSFVVQF','WsRWhgVqRtfvwcddhc','bdlDwznhnNlffMcPTPfzzQ','srCRGRrZCmVTBfBBfTQcZb','brSrrGvRVvWmRsrHrWSbjNJwdDFhnNlwtlnSdnhN','QQqqRfdQQSdjgPmZfBmmPgRhphphJtLmJhTJJhVbTtLhTb','vvlNGzDDDcslcsGDlWHtCFVpcCbThFTtbJFtCh','DrMGlzMVwNGWsWMHDMvlzlMfZdQdQPZfSZRfdrPBfqRZgj','qVHfHNJCHVvvFFbfFlHHnCQQDhLnhhhPZrZnPZPn','mSMszWRMQmhqrnZL','GjtzjSSdRGSjsRtdRMttgGgsqqFNfFcGVvVVvlbHFFGFVFwb'];
+
+let itemPriority = (input) => {
+    if (!input) return 0;
+    const ascii = input.charCodeAt(0);
+    if (ascii >= 97) {
+        // lowercase
+        return ascii - 96;
+    } else {
+        return ascii - 38;
+    }
+};
+
+let sum = 0;
+
+for (let j=0;j<input.length;j+=3){
+    const elf1 = input[j];
+    const elf2 = input[j+1];
+    const elf3 = input[j+2];
+
+    let seen = {};
+
+    // this is so bad lolol
+    for (let i=0;i<Math.max(elf1.length, elf2.length, elf3.length);i++){
+        // lazy man's loop, can have undefined entries
+        const e1 = elf1[i];
+        const e2 = elf2[i];
+        const e3 = elf3[i];
+
+        if (seen[e1]) {
+            seen[e1][0]+= 1;
+        } else {
+            seen[e1] = [1,0,0];
+        }
+        if (seen[e2]) {
+            seen[e2][1]+=1;
+        } else {
+            seen[e2] = [0,1,0];
+        }
+        if (seen[e3]) {
+            seen[e3][2]+=1;
+        } else {
+            seen[e3] = [0,0,1];
+        }
+
+        if (e1 && seen[e1][1] >= 1 && seen[e1][2] >= 1){
+            sum += itemPriority(e1);
+            break;
+        }
+        if (e2 && seen[e2][0] >= 1 && seen[e2][2] >= 1){
+            sum += itemPriority(e2);
+            break;
+        }
+        if (e3 && seen[e3][0] >= 1 && seen[e3][1] >= 1){
+            sum += itemPriority(e3);
+            break;
+        }
+    }
+}
+
+console.log(sum);
